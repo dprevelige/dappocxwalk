f1398d854c69957b866fd4c56e31f7e1a63e68cf
dprevelige <dpreveli@adobe.com>
2025-05-20 16:58:37 -0400

    changing extasset to use custom component

--- blocks/extasset/extasset.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-    const [quoteWrapper] = block.children;
-  
-    const blockquote = document.createElement('notneeded');
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceChildren(blockquote);
-  }
